784f4056a2a60c30af6c95ed66543bd0d765c309
unknown <fabianasoliz2009@cesanarafel.org>
2025-12-02 15:18:05 -0400

    nuevo

--- js/main.js
+++ js/main.js
@@ -1,20 +1,52 @@
-fetch("../data/productos.json")
+let rutaJSON = "";
+
+if (location.pathname.includes("page")) {
+    rutaJSON = "../data/datos.json";
+} else {
+    rutaJSON = "data/datos.json";
+}
+
+// Cargar productos
+fetch(rutaJSON)
     .then(res => res.json())
-    .then(data => mostrarProductos(data));
+    .then(productos => {
+        mostrarProductos(productos);
+        mostrarProductosPagina(productos);
+    })
+    .catch(err => console.log("Error cargando JSON:", err));
 
 function mostrarProductos(productos) {
     const contenedor = document.getElementById("contenedor-productos");
-    productos.forEach(p => {
-        const card = document.createElement("div");
-        card.classList.add("card");
+    if (!contenedor) return;
 
-        card.innerHTML = `
+    productos.slice(0, 3).forEach(p => {
+        const tarjeta = document.createElement("div");
+        tarjeta.className = "tarjeta";
+        tarjeta.innerHTML = `
             <img src="${p.imagen}" alt="${p.nombre}">
             <h3>${p.nombre}</h3>
             <p>${p.descripcion}</p>
-            <strong>${p.precio}</strong>
+            <p><strong>${p.precio}</strong></p>
         `;
-        contenedor.appendChild(card);
+        contenedor.appendChild(tarjeta);
     });
 }
+
+function mostrarProductosPagina(productos) {
+    const lista = document.getElementById("lista-productos");
+    if (!lista) return;
+
+    productos.forEach(p => {
+        const tarjeta = document.createElement("div");
+        tarjeta.className = "tarjeta";
+        tarjeta.innerHTML = `
+            <img src="${p.imagen}" alt="${p.nombre}">
+            <h3>${p.nombre}</h3>
+            <p>${p.descripcion}</p>
+            <p><strong>${p.precio}</strong></p>
+        `;
+        lista.appendChild(tarjeta);
+    });
+}
+
 
